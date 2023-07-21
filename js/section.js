3cae504543c01b081513f025688db2d207ad4859
mishelldc <126489707+mishelldc@users.noreply.github.com>
2023-07-21 12:46:42 -0600

    Add files via upload

---
+++ js/section.js
@@ -0,0 +1,25 @@
+let section_DOM= document.querySelector(".section");
+let section =`
+
+<div class="contenedor">
+
+<div class="co">
+<p class="motito" > Moto </p>
+<p class="motito1" > 🛵 </p>
+</div>
+
+<textarea placeholder="Escribe aquí"></textarea>
+
+<textarea class="pp" placeholder="Escribe aquí"></textarea>
+
+
+<button> Haz clic</button>
+</div> 
+ 
+
+
+
+
+`;
+
+section_DOM.innerHTML = section;
